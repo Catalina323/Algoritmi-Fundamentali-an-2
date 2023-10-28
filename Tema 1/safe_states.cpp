9525d8f768dac3f851ea7d60369e86d317e8763c
Catalina323 <116120497+Catalina323@users.noreply.github.com>
2023-10-28 15:46:12 +0300

    Update safe_states.cpp

--- Tema 1/safe_states.cpp
+++ Tema 1/safe_states.cpp
@@ -3,61 +3,83 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
 
        queue<int> q;
-       int n = 0, q_node;
+       int q_node, n = 0;
        //graph = graph_out      nodurile in care se duce nodul curent
-       vector<vector<int>> graph_in     //nodurile care intra in nodul curent
+       vector<int> graph_in[10001];     //nodurile care intra in nodul curent
        vector<int> safe_nodes;
        bool node_ok;
+       int vis[10001];
 
        for(auto conns: graph)
        {
+           vis[n] = 0;
            n++;
            if(conns.empty())
             {
                 q.push(n-1);
                 safe_nodes.push_back(n-1);
+                vis[n-1] = 1;
+                
             }
-            for(auto i: conns)
+            else
             {
-                graph_in[i].push_back(n-1);
+                for(auto i: conns)
+                {
+                    graph_in[i].push_back(n-1);
+                }
             }
+
         }
 
        while(!q.empty())
        {
            q_node = q.front();
            q.pop();
+           
+           
+        
            if(graph[q_node].empty())
            {
-               for(auto graph_in_node: graph_in[q_node])
+                for(auto graph_in_node: graph_in[q_node])
                 {
                     q.push(graph_in_node);
-                }
+                }   
            }
            else
            {
                 node_ok = true;
                 for(auto graph_node: graph[q_node])
                 {
-                    if(find(safe_nodes.begin(), safe_nodes.end(), graph_node) == safe_nodes.end())
+                   if(vis[graph_node] != 1)
                     {
                         node_ok = false;
                     }
                 }
 
+
                 if(node_ok)
                 {
-                    safe_nodes.push_back(q_node);
-                    for(auto n: graph_in[q_node])
+
+                    if(vis[q_node] != 1)
+                    {
+                        safe_nodes.push_back(q_node);
+                        vis[q_node] = 1;
+                    }
+
+                    for(auto i: graph_in[q_node])
                     {
                         q.push(i);
                     }
                 }
-
             }
-
         }
 
+        for(int z=0; z<n;z++)
+        {
+            cout<<vis[z]<<" ";
+        }
+
+        sort(safe_nodes.begin(), safe_nodes.end());
         return safe_nodes;
         
     }
